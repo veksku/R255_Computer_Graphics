b61c0829c0385a8397a0b90900c3ebe02c42dbbf
Veljko Kučinar <57194423+veksku@users.noreply.github.com>
2021-08-01 04:06:05 +0200

    Add files via upload

---
+++ projekat/cmake-build-debug/libs/imgui/CMakeFiles/imgui.dir/DependInfo.cmake
@@ -0,0 +1,42 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/veljko/Desktop/projekat/libs/imgui/src/imgui.cpp" "/home/veljko/Desktop/projekat/cmake-build-debug/libs/imgui/CMakeFiles/imgui.dir/src/imgui.cpp.o"
+  "/home/veljko/Desktop/projekat/libs/imgui/src/imgui_demo.cpp" "/home/veljko/Desktop/projekat/cmake-build-debug/libs/imgui/CMakeFiles/imgui.dir/src/imgui_demo.cpp.o"
+  "/home/veljko/Desktop/projekat/libs/imgui/src/imgui_draw.cpp" "/home/veljko/Desktop/projekat/cmake-build-debug/libs/imgui/CMakeFiles/imgui.dir/src/imgui_draw.cpp.o"
+  "/home/veljko/Desktop/projekat/libs/imgui/src/imgui_impl_glfw.cpp" "/home/veljko/Desktop/projekat/cmake-build-debug/libs/imgui/CMakeFiles/imgui.dir/src/imgui_impl_glfw.cpp.o"
+  "/home/veljko/Desktop/projekat/libs/imgui/src/imgui_impl_opengl3.cpp" "/home/veljko/Desktop/projekat/cmake-build-debug/libs/imgui/CMakeFiles/imgui.dir/src/imgui_impl_opengl3.cpp.o"
+  "/home/veljko/Desktop/projekat/libs/imgui/src/imgui_tables.cpp" "/home/veljko/Desktop/projekat/cmake-build-debug/libs/imgui/CMakeFiles/imgui.dir/src/imgui_tables.cpp.o"
+  "/home/veljko/Desktop/projekat/libs/imgui/src/imgui_widgets.cpp" "/home/veljko/Desktop/projekat/cmake-build-debug/libs/imgui/CMakeFiles/imgui.dir/src/imgui_widgets.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "IMGUI_IMPL_OPENGL_LOADER_GLAD"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../libs/imgui/include"
+  "../libs/glad/include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/veljko/Desktop/projekat/cmake-build-debug/libs/glad/CMakeFiles/glad.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
